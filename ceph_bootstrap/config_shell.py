5041c9c0481c23f803fdf2accbf7a04647c5f162
Ricardo Marques <rimarques@suse.com>
2020-01-27 15:33:44 +0000

    configshell: Dashboard password must be hidden
    Fixes: https://github.com/SUSE/ceph-bootstrap/issues/48
    
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- ceph_bootstrap/config_shell.py
+++ ceph_bootstrap/config_shell.py
@@ -343,6 +343,7 @@ CEPH_BOOTSTRAP_OPTIONS = {
                     'password': {
                         'default': None,
                         'default_text': 'randomly generated',
+                        'sensitive': True,
                         'handler': PillarHandler('ceph-salt:dashboard:password')
                     },
                     'username': {
@@ -492,6 +493,8 @@ class OptionNode(configshell.ConfigNode):
             if 'handler' in self.option_dict:
                 value, val_type = self.option_dict['handler'].value()
             if value is not None:
+                if self.option_dict.get('sensitive', False):
+                    return '***', None
                 return value, val_type
             if 'default_text' in self.option_dict:
                 return self.option_dict['default_text'], None
