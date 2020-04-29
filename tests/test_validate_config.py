a87c5be4ae0c4fb14d2462afc5b53cbd43aa0423
Nathan Cutler <ncutler@suse.com>
2020-04-29 16:34:05 +0200

    time: support time server not managed by ceph-salt
    With this patch, users can specify an arbitrary time_server_host. If the
    time_server_host is a minion managed by ceph-salt, it will be configured
    as before. If it is *not* a minion managed by ceph-salt, all the minions
    will be pointed at it.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- tests/test_validate_config.py
+++ tests/test_validate_config.py
@@ -14,12 +14,12 @@ class ValidateConfigTest(SaltMockTestCase):
         super(ValidateConfigTest, self).tearDown()
         PillarManager.reload()
 
-    def test_no_boostrap_minion(self):
+    def test_no_bootstrap_minion(self):
         PillarManager.reset('ceph-salt:bootstrap_minion')
         self.assertEqual(validate_config([]), "No bootstrap minion specified in config")
         self.assertEqual(validate_config([{'hostname': 'node1'}]), None)
 
-    def test_boostrap_minion_is_not_admin(self):
+    def test_bootstrap_minion_is_not_admin(self):
         PillarManager.set('ceph-salt:minions:admin', [])
         self.assertEqual(validate_config([]), "Bootstrap minion must be 'Admin'")
         self.assertEqual(validate_config([{'hostname': 'node1'}]), None)
@@ -40,6 +40,24 @@ class ValidateConfigTest(SaltMockTestCase):
         PillarManager.reset('ceph-salt:time_server:subnet')
         self.assertEqual(validate_config([]), "No time server subnet specified in config")
 
+    def test_no_external_time_servers(self):
+        PillarManager.reset('ceph-salt:time_server:external_time_servers')
+        self.assertEqual(validate_config([]), "No external time servers specified in config")
+
+    def test_time_server_not_a_minion(self):
+        not_minion_err = ('Time server is not a minion: {} '
+                          'setting will not have any effect')
+        PillarManager.set('ceph-salt:time_server:server_host', 'foo.example.com')
+        PillarManager.reset('ceph-salt:time_server:external_time_servers')
+        self.assertEqual(
+            validate_config([]),
+            not_minion_err.format('time server subnet'))
+        PillarManager.reset('ceph-salt:time_server:subnet')
+        PillarManager.set('ceph-salt:time_server:external_time_servers', ['pool.ntp.org'])
+        self.assertEqual(
+            validate_config([]),
+            not_minion_err.format('external time servers'))
+
     def test_admin_not_cluster_minion(self):
         PillarManager.set('ceph-salt:bootstrap_minion', 'node3.ceph.com')
         PillarManager.set('ceph-salt:minions:admin', ['node3.ceph.com'])
@@ -56,7 +74,9 @@ class ValidateConfigTest(SaltMockTestCase):
     def create_valid_config(cls):
         PillarManager.set('ceph-salt:bootstrap_minion', 'node1.ceph.com')
         PillarManager.set('ceph-salt:bootstrap_mon_ip', '10.20.188.201')
+        PillarManager.set('ceph-salt:time_server:enabled', True)
         PillarManager.set('ceph-salt:time_server:server_host', 'node1.ceph.com')
+        PillarManager.set('ceph-salt:time_server:external_time_servers', ['pool.ntp.org'])
         PillarManager.set('ceph-salt:time_server:subnet', '10.20.188.0/24')
         PillarManager.set('ceph-salt:minions:all', ['node1.ceph.com', 'node2.ceph.com'])
         PillarManager.set('ceph-salt:minions:admin', ['node1.ceph.com'])
