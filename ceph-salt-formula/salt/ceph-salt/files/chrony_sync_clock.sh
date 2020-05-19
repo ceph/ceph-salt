1d2ea8a55b7d70404bf8957873b759a90416b04f
Nathan Cutler <ncutler@suse.com>
2020-05-19 17:07:59 +0200

    chrony_sync_clock.sh: run first chronyc in try-wait loop
    Fixes: https://github.com/ceph/ceph-salt/issues/238
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- ceph-salt-formula/salt/ceph-salt/files/chrony_sync_clock.sh
+++ ceph-salt-formula/salt/ceph-salt/files/chrony_sync_clock.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-chronyc 'burst 4/4'
+# address https://github.com/ceph/ceph-salt/issues/238
+# by waiting up to 5 minutes for chrony to see its sources
+for trywait in "$(seq 0 9)" ; do
+    chronyc 'burst 4/4' && break || true
+    sleep 30
+done
 sleep 15
 chronyc makestep
 chronyc waitsync 60 0.04
