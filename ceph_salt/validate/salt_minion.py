d180981427fc2bb4f4443a6a97b624016ad44382
Ricardo Marques <rimarques@suse.com>
2020-09-18 13:18:25 +0100

    Fix salt warn when 'ceph-salt config' is executed for the first time
    The following message will no longer be displayed on first
    'ceph-salt config' execution:
    
    "No minions matched the target. No command was sent, no jid was assigned."
    
    Fixes: https://github.com/ceph/ceph-salt/issues/393
    
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- ceph_salt/validate/salt_minion.py
+++ ceph_salt/validate/salt_minion.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from ..exceptions import ValidationException
 from ..salt_utils import SaltClient
 
@@ -11,7 +13,8 @@ class UnableToSyncAll(ValidationException):
 
 
 def sync_all():
-    result = SaltClient.local_cmd('ceph-salt:member', 'saltutil.sync_all', tgt_type='grain')
+    with contextlib.redirect_stdout(None):
+        result = SaltClient.local_cmd('ceph-salt:member', 'saltutil.sync_all', tgt_type='grain')
     for minion, value in result.items():
         if not value:
             raise UnableToSyncAll()
