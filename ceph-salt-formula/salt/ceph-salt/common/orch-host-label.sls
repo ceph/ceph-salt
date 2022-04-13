ceph/ceph-salt

commit e191159cbc88873894df960429e64a728841aad2

add the orchestrator `_admin` host label during `ceph-salt update`
320e2d4e08248569b15ebe4e372a4e76afdfeea6 introduced the concept of
adding orchestrator host labels during `ceph-salt apply`, but
existing clusters will have already added hosts without this label

Fixes: https://bugzilla.suse.com/show_bug.cgi?id=1197188
Signed-off-by: Michael Fritch <mfritch@suse.com>

ceph-salt-formula/salt/ceph-salt/common/orch-host-label.sls
@@ -1,0 +1,20 @@
+{% import 'macros.yml' as macros %}
+
+{% set my_hostname = salt['ceph_salt.hostname']() %}
+
+{% if 'admin' in grains['ceph-salt']['roles'] %}
+
+{{ macros.begin_stage('Add host labels to ceph orchestrator') }}
+add _admin host label to ceph orch:
+  ceph_orch.add_host_label:
+    - host: {{ my_hostname }}
+    - label: {{ '_admin' }}
+    - failhard: True
+{{ macros.end_stage('Add host labels to ceph orchestrator') }}
+
+{% else %}
+
+not an admin role:
+  test.nop
+
+{% endif %}

ceph-salt-formula/salt/_states/ceph_orch.py
@@ -101,6 +101,25 @@ def add_host(name, host, ipaddr, is_admin=False):
         ret['comment'] = cmd_ret.get('stderr')
     return ret
 
+def add_host_label(name, host, label):
+    """
+    Requires the following grains to be set:
+      - ceph-salt:execution:admin_host
+    """
+    ret = {'name': name, 'changes': {}, 'comment': '', 'result': False}
+    admin_host = __salt__['grains.get']('ceph-salt:execution:admin_host')
+    cmd_ret = __salt__['ceph_salt.ssh'](
+                       admin_host,
+                       "sudo ceph orch host label add {} {}".format(
+                           host,
+                           label,
+                        ),
+                       attempts=10)
+    if cmd_ret['retcode'] == 0:
+        ret['result'] = True
+    else:
+        ret['comment'] = cmd_ret.get('stderr')
+    return ret
 
 def rm_clusters(name):
     """

ceph-salt-formula/salt/ceph-salt/common/sshkey.sls
@@ -14,6 +14,7 @@ configure sudoers:
     - text:
       - "cephadm ALL=NOPASSWD: /usr/bin/ceph -s"
       - "cephadm ALL=NOPASSWD: /usr/bin/ceph orch host add *"
+      - "cephadm ALL=NOPASSWD: /usr/bin/ceph orch host label *"
       - "cephadm ALL=NOPASSWD: /usr/bin/ceph orch host ok-to-stop *"
       - "cephadm ALL=NOPASSWD: /usr/bin/ceph orch status --format=json"
       - "cephadm ALL=NOPASSWD: /usr/bin/python3"

ceph-salt-formula/salt/ceph-salt/update/update-end.sls
@@ -1,5 +1,6 @@
 include:
     - ..common.sshkey-cleanup
+    - ..common.orch-host-label
 
 set updated:
   grains.present:

commit ccdc8a67358f6186fd3fbe104d9f09346c3f43bb

Only add _admin host labels during update if cluster is deployed
Fixes: e191159cbc88873894df960429e64a728841aad2
Fixes: https://bugzilla.suse.com/show_bug.cgi?id=1198428
Signed-off-by: Tim Serong <tserong@suse.com>

ceph-salt-formula/salt/ceph-salt/common/orch-host-label.sls
@@ -4,6 +4,7 @@
 
 {% if 'admin' in grains['ceph-salt']['roles'] %}
 
+{% if pillar['ceph-salt'].get ('execution', {}).get('deployed') != False %}
 {{ macros.begin_stage('Add host labels to ceph orchestrator') }}
 add _admin host label to ceph orch:
   ceph_orch.add_host_label:
@@ -11,6 +12,7 @@ add _admin host label to ceph orch:
     - label: {{ '_admin' }}
     - failhard: True
 {{ macros.end_stage('Add host labels to ceph orchestrator') }}
+{% endif %}
 
 {% else %}
 